acb54a0a3d4d48233b621d7206717460982f990f
Marcelo Alaniz <alanizmarcelo@gmail.com>
2017-05-23 19:17:35 -0300

    initial app: gists

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,60 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Hola from './components/Hola'
+import { BrowserRouter as Router, Route, Link }  from 'react-router-dom';
 
-ReactDOM.render(<Hola />, document.getElementById('root'))
+
+
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			gists: null
+		}
+	}
+	componentDidMount() {
+		fetch('https://api.github.com/gists')
+		.then(res => res.json())
+		.then(gists => this.setState({
+			gists
+		}));
+	}
+	render () {
+		const styleSidebar = {
+			width: '300px',
+			backgroundColor: '#ccc',
+			float: 'left'
+		}
+		const styleContent = {
+			width: '500px',
+			float: 'left'
+		}
+		const { gists } = this.state;
+		return (
+			<Router>
+				<div>
+					<div style={styleSidebar}> 
+						<h1> Lista de Gits </h1>
+						<ul>
+							{ gists ? gists.map((x ,i) => (
+								<li key={i}> 
+									<Link to={`/g/${x.id}`}>
+										{x.description || '[ no hay description]'} 
+									</Link>
+								</li>) 
+							) : (<p> loading ... </p>)
+							}
+						</ul>
+					</div>
+					<div style={styleContent}>
+						<h1> Detalle </h1>
+						<Route path="/g/:gistId" render={({match}) => {
+							return <h1> {gists.find(g => g.id === match.params.gistId).id} </h1>
+						}} />
+					</div>
+				</div>
+			</Router>
+		)
+	}
+}
+ReactDOM.render(<App />, document.getElementById('root'))
 
